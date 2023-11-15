a7c8c7fb3a36cce761057049b4f793cdc543f749
Ziad Ismael <117987711+ziadismael@users.noreply.github.com>
2023-11-15 02:23:46 +0200

    runMachine() function in Machine.cpp

--- Machine.cpp
+++ Machine.cpp
@@ -58,6 +58,106 @@ void Machine::clearRegister()
 	cout << "Registers has been cleared." << endl;
 }
 
+void Machine::runMachine()
+{
+	string cellInstruction = memory.cells[starting_address];
+	cellInstruction += memory.cells[starting_address + 1];
+	while (cellInstruction != "0000" || starting_address <= 255) {
+
+		PC = starting_address;
+		IR = cellInstruction;
+
+		char opcode;
+		opcode = cellInstruction[0];
+
+		string registernum;
+		registernum = cellInstruction[1];
+		int registerNum = stoi(registernum, nullptr, 16);
+
+		string x;
+		x = cellInstruction[2];
+		int X = stoi(x, nullptr, 16);
+
+		string y;
+		y = cellInstruction[3];
+		int Y = stoi(y, nullptr, 16);
+
+		//XY
+		string xy = cellInstruction.substr(2);
+		int XY = stoi(xy, nullptr, 16);
+
+		string s0, sr;
+
+		Instruction* op_one = nullptr, * op_two, * op_three, * op_four, * op_five;
+		
+		if (cellInstruction == "0000") {
+			cout << "Program has ended." << endl;
+			break;
+		}
+		if (starting_address + 2 >= 255) {
+			cout << "Program has ended." << endl;
+			break;
+		}
+
+		switch (opcode)
+		{
+		case '1':
+
+			op_one = new LoadInstructionM(registers[registerNum], memory, XY);
+			op_one->excute();
+			break;
+
+		case '2':
+
+			op_two = new LoadInstructiond(xy, registers[registerNum]);
+			op_two->excute();
+			break;
+
+		case '3':
+
+			op_three = new StoreatMemory(XY, registers[registerNum], memory);
+			op_three->excute();
+			break;
+
+		case '4':
+
+			op_four = new MovetoRegister(registers[X], registers[Y]);
+			op_four->excute();
+			break;
+
+		case '5':
+
+			op_five = new AddComopliments(registers[X], registers[Y], registers[registerNum], alu);
+			op_five->excute();
+			break;
+
+		case 'B':
+			s0 = registers[0].getValue();
+			sr = registers[registerNum].getValue();
+			if (s0 == sr)
+			{
+				starting_address = XY;
+				cellInstruction = memory.cells[starting_address];
+				cellInstruction += memory.cells[starting_address + 1];
+				continue;
+			}
+			break;
+		case 'C':
+			cellInstruction = "0000";
+			cout << "program has ended." << endl;
+			continue;
+			break;
+
+		default:
+			break;
+		}
+		
+		starting_address += 2;
+		cellInstruction = memory.cells[starting_address];
+		cellInstruction += memory.cells[starting_address + 1];
+	}
+}
+
 void Machine::displayBgad()
 {	
 	int Screen_Ascii = stoi(memory.cells[0], nullptr, 16);
